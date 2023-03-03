6f17aba452a31de6380d484f10fe5270948a6ac5
Abhishek Rana <96675677+AbhishekRana21@users.noreply.github.com>
2023-03-03 11:48:32 +0530

    Updated Q2 Solution

--- Week 2/Q2_Solution.cpp
+++ Week 2/Q2_Solution.cpp
@@ -3,19 +3,6 @@
 #include <cstdlib>
 using namespace std;
 
-void SelectionSort(int *arr, int n){
-    int i, j, temp;
-    for(i = 0; i<n-1; ++i){
-        for(j = i+1; j<n; ++j){
-            if(arr[i] > arr[j]){
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-    }
-}
-
 int main() {
     int tc;
     cin>>tc;
@@ -26,7 +13,6 @@ int main() {
         int i,j,k, flag = 0;
         for(i = 0; i<n; ++i)
             cin>>arr[i];
-        SelectionSort(arr,n);
         for(i=0; i<n-2; ++i){
             for(j=i+1; j<n-1; ++j){
                 for(k=j+1; k<n; ++k){
@@ -36,9 +22,7 @@ int main() {
                         break;
                     }
                 }
-                if(flag) break;
             }
-            if(flag) break;
         }
         if(!flag){
             cout<<"No sequence found.";
